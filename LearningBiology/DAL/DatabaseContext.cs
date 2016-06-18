0c972377e65eaceadc2dda2330a20e492a1b95d1
marijanikolova <marijanikolova93@hotmail.com>
2016-06-18 14:14:31 +0200

    DatabaseContext created and changes in model.

---
+++ LearningBiology/DAL/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using LearningBiology.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LearningBiology.DAL
+{
+    public class DatabaseContext:DbContext
+    {
+        public DatabaseContext() : base("DatabaseContext")
+        {
+
+        }
+
+        public DbSet<Section> Sections { get; set;  }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<OfferedAswer> OfferedAnswers { get; set; }
+    }
+}
--- LearningBiology/Models/Question.cs
+++ LearningBiology/Models/Question.cs
@@ -5,11 +5,17 @@ using System.Web;
 
 namespace LearningBiology.Models
 {
+    public enum Type
+    {
+        OneChoice, MultipleAnswers, DragAndDrop
+    }
+
     public class Question
     {
         public int ID { get; set; } 
         public string text { get; set; }
         public int sectionID { get; set; }
+        public Type? Type { get; set; }
 
         public virtual Section Section { get; set; }
         public virtual ICollection<OfferedAswer> OfferedAnswers { get; set; }
